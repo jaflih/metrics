d9cbed5ceb669a4b073c3afe2e2a36e590f2ddac
jihaneh <jijih@hotmail.fr>
2022-03-09 15:49:26 +0100

    Update routes

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<SymbolPage />} />
-        <Route path="/company/:name" element={<Profile />} />
-        <Route path="*" element={<NotMatch />} />
+        <Route path="/metrics/" element={<SymbolPage />} />
+        <Route path="/metrics/company/:name" element={<Profile />} />
+        <Route path="/metrics/*" element={<NotMatch />} />
       </Routes>
     </div>
   );
